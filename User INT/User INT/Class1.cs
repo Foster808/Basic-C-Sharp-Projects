453c5dc3c72366aa3bb58644258c36f1c96410cc
Lara Foster <LaFoster@outlook.com>
2020-03-15 12:15:08 -0700

    Add User INT

---
+++ User INT/User INT/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_INT
+{
+    class Class1
+    {
+        //public Class1 myObj = new Class1();
+        public static int userInt { get; set; }
+        
+    }
+}
