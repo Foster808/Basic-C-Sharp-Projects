373f4c7c7d0b74b06091bd7f4f2911621b0a1e3d
Lara Foster <LaFoster@outlook.com>
2020-03-18 18:27:57 -0700

    Update User INT 2

--- User INT 2/User INT 2/Pass.cs
+++ User INT 2/User INT 2/Pass.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.InteropServices;
 
 namespace User_INT_2
 {
     public class Pass
     {
-        public void MyMethod(int x, [Optional] int y)
+        public void MyMethod(int x, int y = 8)
+        //public void MyMethod(int x, int y = 8);
         {
+            
+
             int input = x;
             int input2 = y;
 
             Console.WriteLine("Your numbers added: " + (x + y));
-
+            
         }
+
+        
+       
     }
 }
--- User INT 2/User INT 2/Program.cs
+++ User INT 2/User INT 2/Program.cs
@@ -15,9 +15,21 @@ namespace User_INT_2
             Console.WriteLine("Please enter two separate numbers. One at a time. Also enter nothing (not zero) as the second number.");
             int userInt = Convert.ToInt32(Console.ReadLine());
             int userInt2 = Convert.ToInt32(Console.ReadLine());
+         
+            
+            if (userInt2 > 0)
+            {
+                Console.WriteLine("You entered a number. We asked you not to.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+            Console.ReadLine();
             obj1.MyMethod(userInt, userInt2);
-
             Console.ReadLine();
         }
+
     }
 }
