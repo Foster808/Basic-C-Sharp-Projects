2b63d5f14d545e766dcc557a15419be4fb44bb5b
Lara Foster <LaFoster@outlook.com>
2020-03-18 17:12:00 -0700

    Add User INT 2

---
+++ User INT 2/User INT 2/Pass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace User_INT_2
+{
+    public class Pass
+    {
+        public void MyMethod(int x, [Optional] int y)
+        {
+            int input = x;
+            int input2 = y;
+
+            Console.WriteLine("Your numbers added: " + (x + y));
+
+        }
+    }
+}
---
+++ User INT 2/User INT 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_INT_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pass obj1 = new Pass();
+
+            Console.WriteLine("Please enter two separate numbers. One at a time. Also enter nothing (not zero) as the second number.");
+            int userInt = Convert.ToInt32(Console.ReadLine());
+            int userInt2 = Convert.ToInt32(Console.ReadLine());
+            obj1.MyMethod(userInt, userInt2);
+
+            Console.ReadLine();
+        }
+    }
+}
