25ace8b2d95b95ad87a180b8485bc007544c6efc
Lara Foster <LaFoster@outlook.com>
2020-03-18 17:27:47 -0700

    Add User INT 3

---
+++ User INT 3/User INT 3/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_INT_3
+{
+    public class Class1
+    {
+        public void MyMethod(int x, int y)
+        {
+            int input = x;
+            int input2 = y;
+
+            Console.WriteLine("Your first number with math: " + (x * 11));
+            Console.WriteLine("Your second number is " + y);
+
+        }
+    }
+}
---
+++ User INT 3/User INT 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_INT_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj1 = new Class1();
+
+            Console.WriteLine("Please enter two separate numbers. One at a time.");
+            int userInt = Convert.ToInt32(Console.ReadLine());
+            int userInt2 = Convert.ToInt32(Console.ReadLine());
+            obj1.MyMethod(userInt, userInt2);
+            Console.WriteLine("Thanks. We've multiplied your first number by 11, and just shown you your second number whole.");
+
+            Console.ReadLine();
+        }
+    }
+}
