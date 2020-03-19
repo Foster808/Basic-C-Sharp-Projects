29336198350d28e2659fcc4fda0c0740b6f5b372
Lara Foster <LaFoster@outlook.com>
2020-03-19 11:45:33 -0700

    Add Divide by 2

---
+++ Divide by 2/Divide by 2/Divider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divide_by_2
+{
+    class Divider
+    {
+        public void MyMethod(int x)
+        {
+            int input = x;
+
+            Console.WriteLine("Your number divided by 2 is: " + (x / 2));
+            Console.ReadLine();
+        }
+        static void MyMethod(int x, int y)
+        {
+            Console.WriteLine("MyMethod(int x, int y)");
+
+            //Console.WriteLine("Your number divided by 2 is: " + (x / 2));
+            //Console.ReadLine();
+        }
+
+    }
+}
---
+++ Divide by 2/Divide by 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divide_by_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Divider obj1 = new Divider();
+            
+            Console.WriteLine("Please enter a number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            obj1.MyMethod(userNum);
+            //Console.WriteLine("Great. Here's your number divided by 2: " + (userNum / 2));
+            Console.ReadLine();
+            
+        }
+            //public Divider obj1 = new Divider(); 
+        }
+    }
+
