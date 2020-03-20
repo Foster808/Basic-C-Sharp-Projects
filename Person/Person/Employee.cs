3f46b3e62e57a3bc6dd72fbe72e084d6b6fd3cd9
Lara Foster <LaFoster@outlook.com>
2020-03-20 11:31:17 -0700

    Add Person

---
+++ Person/Person/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Employee : Person
+    {
+        public void Id(int[] args)
+        {
+                Person myObj = new Person();
+                myObj.FirstName = "Sample";
+                myObj.LastName = "Student";
+                Console.WriteLine(myObj.FullName);
+            }
+        }
+    }
+
---
+++ Person/Person/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+            person.FullName = new List<string>() { "Sample Student" };
+            person.SayName();
+            Console.ReadLine();
+        }
+    }
+}
