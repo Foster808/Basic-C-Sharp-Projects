247b83862c238e0928d83204f0d458cd5a9f6c3c
Lara Foster <LaFoster@outlook.com>
2020-03-21 10:47:08 -0700

    Add Person Two

---
+++ Person Two/Person Two/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Two
+{
+    class Employee : Person2
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Saying a name...");
+        }
+    }
+}
---
+++ Person Two/Person Two/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+
+            person.SayName();
+
+            Console.WriteLine("Name: " + person.firstName + person.lastName);
+            Console.ReadLine();
+        }
+    }
+}
