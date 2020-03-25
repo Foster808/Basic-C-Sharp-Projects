a7f3ca867da79931bcc8c950f47103f9c3011b27
Lara Foster <LaFoster@outlook.com>
2020-03-24 19:12:19 -0700

    Add Struct

---
+++ Struct/Struct/Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+
+        //protected decimal Amount;
+
+        public void DecAmount()
+        {
+            Amount = Decimal.Add(Amount, 3.8m);
+        }
+
+
+        //have it be of data type decimal -- decimal b = 3.8m;
+    }
+}
---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number dec1 = new Number();
+
+            //int dec1 = Convert.ToInt32(Console.ReadLine())
+
+
+            Console.WriteLine(dec1);
+            Console.ReadLine();
+
+            //Console.WriteLine(DecAmount(Amount)); 
+        }
+
+            
+
+            
+        
+        }
+    }
+
+
