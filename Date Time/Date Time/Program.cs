19feaec2c8fda5b851b23816b6f6d3b65f5d2c2e
Lara Foster <LaFoster@outlook.com>
2020-03-28 10:56:15 -0700

    Update Date Time

--- Date Time/Date Time/Program.cs
+++ Date Time/Date Time/Program.cs
@@ -11,22 +11,35 @@ namespace Date_Time
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Right now, our current time is: ");
-            Console.WriteLine(DateTime.Now);
-            
-            int userInput;
-            Console.WriteLine("Please enter a number.");
-            userInput = int.Parse(Console.ReadLine());
+
+            decimal userInput;
+            Console.WriteLine("Please enter a number from 1-9.");
+            userInput = decimal.Parse(Console.ReadLine());
             Console.WriteLine("You said, " + userInput);
             Console.ReadLine();
 
-            DateTime ourTime = DateTime.Now;
+            var date01 = DateTime.Now;
+            Console.WriteLine(date01);
+
+            var resultTime = date01.AddHours(userInput);
+            Console.WriteLine(resultTime);
             
-            DateTime theirTime = userInput;
+            
+            
+            //Console.WriteLine("Right now, our current time is: ");
+            //Console.WriteLine(DateTime.Now);
+            
+            
+
+            //DateTime ourTime = DateTime.Now;
+
+            //Console.WriteLine("The time plus what you chose is: " resultTime)
+            
+            //DateTime theirTime = userInput;
             //DateTime theirTime = DateTime.Now + userInput;
 
-            TimeSpan resultTime = ourTime + theirTime;
-            Console.WriteLine("This is the actual time plus your number: " + resultTime);
+            //TimeSpan resultTime = ourTime + theirTime;
+            //Console.WriteLine("This is the actual time plus your number: " + resultTime);
             Console.ReadLine();
         }
     }
